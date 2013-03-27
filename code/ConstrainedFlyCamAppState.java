93a7cac16b0c987cb0bc3a8205540dcd3535162d
thecucumbas <sean.tyler.berryhill@gmail.com>
2013-03-26 21:11:52 -0500

    Added new classes
    hey! so i added a few classes, and changed the way ConstraindFlyByCamera
    works. now you will get around 350-400 fps :) im gonna explain how to
    use in the README

---
+++ code/ConstrainedFlyCamAppState.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConstrainedCamera;
+
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.input.FlyByCamera;
+import com.jme3.app.Application;
+
+/**
+ *
+ * @author Roochie
+ */
+public class ConstrainedFlyCamAppState extends AbstractAppState {
+
+    private Application app;
+    private ConstrainedFlyByCamera flyCam;
+
+    public ConstrainedFlyCamAppState() {
+    }    
+
+    /**
+     *  This is called by SimpleApplication during initialize().
+     */
+    void setCamera( ConstrainedFlyByCamera cam ) {
+        this.flyCam = cam;
+    }
+    
+    public ConstrainedFlyByCamera getCamera() {
+        return flyCam;
+    }
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        
+        this.app = app;
+
+        if (app.getInputManager() != null) {
+        
+            if (flyCam == null) {
+                flyCam = new ConstrainedFlyByCamera(app.getCamera());
+            }
+            
+            flyCam.registerWithInput(app.getInputManager());            
+        }               
+    }
+            
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        
+        flyCam.setEnabled(enabled);
+    }
+    
+    @Override
+    public void cleanup() {
+        super.cleanup();
+
+        flyCam.unregisterInput();        
+    }
+
+
+}
+
